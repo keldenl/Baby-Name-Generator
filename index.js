a65cd36124f5c628f2b120ceac99ad4ef2b845fd
keldenl <keldenlin@gmail.com>
2021-07-29 18:42:22 -0700

    replace text with then

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const testList = ['eliza', 'kevin', 'kelden', 'tiajha']
 
 //let nameRaw = fs.readFileSync(path.resolve(__dirname, 'db.txt'), 'utf8')
-let nameRaw = fetch('db.txt').text()
+let nameRaw = fetch('db.txt').then(res => res.text())
 let nameList = nameRaw.split("\n")
 
 String.prototype.toProperCase = function () {
