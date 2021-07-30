aba9eda58ba613a75fb9f3ca4069acd76e8379c9
keldenl <keldenlin@gmail.com>
2021-07-29 18:54:14 -0700

    add sync to everything

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ const getRandSub = () => randNum(2, 4)
 // random word from list
 const getRandWord = () => nameList()[randNum(0, nameList().length)]
 
-const getRandPart = (startIdx) => {
-  const word = getRandWord()
+const getRandPart = async (startIdx) => {
+  const word = await getRandWord()
   const sub = getRandSub()
   if (startIdx > word.length-sub) {
     return ''
@@ -28,11 +28,11 @@ const getRandPart = (startIdx) => {
   return word.substring(randStart, sub+randStart)
 }
 
-const createName = () => {
+const createName = async () => {
   let output = ''
 
   while(output.length < 6) {
-    const name = getRandPart(output.length)
+    const name = await getRandPart(output.length)
     output += name  
   }
  
@@ -40,11 +40,12 @@ const createName = () => {
   return output.toProperCase()
 }
 
-const createList = (max = 10) => {
+const createList = async (max = 10) => {
   let outputList = ''
   for (let i = 0; i < max; i++) {
-    outputList.push(createName())
+    outputList.push(await createName())
   }
+  alert(outputList)
   return outputList
 }
 
